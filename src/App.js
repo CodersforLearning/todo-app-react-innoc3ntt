97b7d491768355b1b9f1103a30087c6a1f10320f
Tim <timothy.tan.93@gmail.com>
2021-11-23 08:21:18 +0800

    work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import "./App.css";
 import React from "react";
+import {Button, Card} from "react-bootstrap";
+import * as PropTypes from "prop-types";
 
-function Card(props) {
-    return <div>
-        <h2>To do </h2>
-        <p>test</p> {props.desc}</div>;
+function ToDo({
+                  toDo,
+                  removeToDo,
+                  index,
+                  markToDo
+              }) {
+    return <div className="toDo"><span>{toDo.text}</span></div>;
 }
 
+ToDo.propTypes = {
+    toDo: PropTypes.shape({
+        text: PropTypes.string,
+        isDone: PropTypes.bool
+    }),
+    removeToDo: PropTypes.func,
+    index: PropTypes.number,
+    markToDo: PropTypes.func
+};
+
 function App() {
-    const [toDos, setTodos] = React.useState([{text: "this is sample", isDone: false}]);
+    const [toDos, setToDos] = React.useState([
+        {
+            text: "this is sample",
+            isDone: false
+        }]);
+
+    const addToDo = text => {
+        const newToDos = [...toDos, {text}];
+        setToDos(newToDos);
+    };
+
+    const markToDoDone = index => {
+        const newTodos = [...toDos];
+        newTodos[index].isDone = true;
+        setToDos(newTodos);
+    };
+
+    const removeToDo = index => {
+        const newToDos = [...toDos];
+        newToDos.splice(index, 1);
+        setToDos(newToDos);
+    };
+
     return (
         <div className="min-h-full">
             <h1 className="text-pink-500">To do list</h1>
-            <input type="text"/>
-            <button type="submit" className="bg-red">ADD</button>
+            <div>
+                {toDos.map((toDo, index) => {
+                    return (<Card>
+                        <Card.Body>
+                            <ToDo key={index} index={index} toDo={toDo} markToDo={markToDoDone}
+                                  removeToDo={removeToDo}/>
+                            <Card.Text>TEST</Card.Text>
+                        </Card.Body>
+                    </Card>);
+                })}
+                <button type="submit" className="bg-red">ADD</button>
+            </div>
         </div>
     );
 }
-
-const addToDo = text => {
-    const newTodos = [...todos, {text}];
-};
 
 export default App;
